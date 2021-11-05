9720a9baf57a233f67344555b9a9626644631747
Edgar Valli <evalli@gentor.com>
2021-11-05 17:12:23 -0600

    Se modifica el url de app_config_b2c.py

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ app.jinja_env.globals.update(
     _build_auth_code_flow=_build_auth_code_flow)  # Used in template
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=80)
+    app.run(debug=False, host='0.0.0.0', port=5000)
